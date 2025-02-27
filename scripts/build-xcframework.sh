15bcd7968ee03a048af432de456c2e4a80295bd6
Francis Chong <francis@goodnotes.com>
2025-02-27 11:02:01 +0000

    include mac catalyst build
    incorporate workaround from Automerge to build for Catalyst

--- scripts/build-xcframework.sh
+++ scripts/build-xcframework.sh
@@ -20,7 +20,23 @@ SWIFT_FOLDER="swift"
 BUILD_FOLDER="target"
 
 XCFRAMEWORK_FOLDER="${FRAMEWORK_NAME}.xcframework"
-# FRAMEWORK_FOLDER="${FRAMEWORK_NAME}.framework"
+
+# currently macabi/Catalyst target has no prebuild rust-std library hence we use `-Z build-std`
+# how to build-std: https://doc.rust-lang.org/nightly/cargo/reference/unstable.html#build-std
+# list of targets with prebuild rust-std https://doc.rust-lang.org/nightly/rustc/platform-support.html
+
+# The specific issue with an earlier nightly version and linking into an
+# XCFramework appears to be resolved with latest versions of +nightly toolchain
+# (as of 10/10/23), but leaving it open to float seems less useful than
+# moving the pinning forward, since Catalyst support (target macabi) still
+# requires an active, nightly toolchain.
+RUST_NIGHTLY="nightly-2024-05-23"
+
+echo "Install nightly and rust-src for Catalyst"
+rustup toolchain install ${RUST_NIGHTLY}
+rustup component add rust-src --toolchain ${RUST_NIGHTLY}
+rustup update
+rustup default ${RUST_NIGHTLY}
 
 echo "▸ Install toolchains"
 rustup target add x86_64-apple-ios # iOS Simulator (Intel)
@@ -62,6 +78,12 @@ echo "▸ Building for x86_64-apple-darwin"
 CFLAGS_x86_64_apple_darwin="-target x86_64-apple-darwin" \
 cargo build --target x86_64-apple-darwin --package "${PACKAGE_NAME}" --locked --release
 
+echo "▸ Building for aarch64-apple-ios-macabi"
+cargo "+${RUST_NIGHTLY}" build -Zbuild-std=std,panic_abort --target aarch64-apple-ios-macabi --package "${PACKAGE_NAME}" --locked --release
+
+echo "▸ Building for x86_64-apple-ios-macabi"
+cargo "+${RUST_NIGHTLY}" build -Zbuild-std=std,panic_abort --target x86_64-apple-ios-macabi --package "${PACKAGE_NAME}" --locked --release
+
 echo "▸ Consolidating the headers and modulemaps for XCFramework generation"
 mkdir -p "${BUILD_FOLDER}/includes/yniffiFFI"
 cp "${SWIFT_FOLDER}/scaffold/yniffiFFI.h" "${BUILD_FOLDER}/includes/yniffiFFI/yniffiFFI.h"
@@ -80,6 +102,13 @@ lipo -create  \
     "./${BUILD_FOLDER}/x86_64-apple-darwin/release/${LIB_NAME}" \
     "./${BUILD_FOLDER}/aarch64-apple-darwin/release/${LIB_NAME}" \
     -output "${BUILD_FOLDER}/apple-darwin/release/${LIB_NAME}"
+
+echo "▸ Lipo (merge) x86 and arm macOS Catalyst static libraries into a fat static binary"
+mkdir -p "${BUILD_FOLDER}/apple-macabi/release"
+lipo -create  \
+    "${BUILD_FOLDER}/x86_64-apple-ios-macabi/release/${LIB_NAME}" \
+    "${BUILD_FOLDER}/aarch64-apple-ios-macabi/release/${LIB_NAME}" \
+    -output "${BUILD_FOLDER}/apple-macabi/release/${LIB_NAME}"
 
 # what docs there are:
 # xcodebuild -create-xcframework -help
